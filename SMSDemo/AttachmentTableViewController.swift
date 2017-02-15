46786faac308854072a40260799d0d81fdb9450b
cice <cice@aula1PovMacN09.local>
2017-02-15 11:48:20 +0100

    Envio de sms mediante aplicacion

--- SMSDemo/AttachmentTableViewController.swift
+++ SMSDemo/AttachmentTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AttachmentTableViewController: UITableViewController {
+class AttachmentTableViewController: UITableViewController, MFMessageComposeViewControllerDelegate {
 
     let filenames = ["10 Great iPhone Tips.pdf", "camera-photo-tips.html", "foggy.jpg", "Hello World.ppt", "no more complaint.png", "Why Appcoda.doc"]
     
@@ -42,6 +43,67 @@ class AttachmentTableViewController: UITableViewController {
         cell.imageView?.image = UIImage(named: "icon\(indexPath.row)");
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedFile = filenames[indexPath.row]
+        sendSMS(attachment: selectedFile)
+        
+        //Envia a la app por defecto
+        //Si le pasamos en URL un tipo sms:y numero , swift sabe que es un mensaje. Es un URLSkin
+        UIApplication.shared.open(URL(string: "sms:1234678")!, options: [:] completionHandler: nil)
+        
+    }
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+            
+            case MessageComposeResult.cancelled:
+                
+                print("SMS cancelado")
+            
+            case MessageComposeResult.failed:
+            
+                let alertMessage = UIAlertController(title: "Error", message: "Fallo en el envio", preferredStyle: .alert)
+                alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                present(alertMessage, animated: true, completion: nil)
+            
+            case MessageComposeResult.sent:
+                print("SMS enviado")
+
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func sendSMS (attachment: String){
+    
+        //Comprobamos si puede enviar SMS
+        guard MFMessageComposeViewController.canSendText() else {
+        //crear alerta
+            let alertMessage = UIAlertController(title: "Error", message: "Fallo en el envio", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertMessage, animated: true, completion: nil)
+            return
+        }
+        
+        //Creamos el sms
+        let messageController = MFMessageComposeViewController()
+        messageController.messageComposeDelegate = self
+        messageController.recipients = ["123456789","0987654321"]
+        messageController.body = "Aquí escribimos el texto a enviar!"
+        
+        
+        //MMS
+        let fileParts = attachment.components(separatedBy: ".")
+        let fileName = fileParts[0]
+        let fileExtension = fileParts[1]
+        let filePath = Bundle.main.path(forResource: fileName, ofType: fileExtension)
+        let fileURL = NSURL.fileURL(withPath: filePath!)
+        
+        //Añadimos el archivo al controlador
+        messageController.addAttachmentURL(fileURL, withAlternateFilename: nil)
+        //Mostramos el controlador
+        present(messageController, animated: true, completion: nil)
+    
     }
     
 
